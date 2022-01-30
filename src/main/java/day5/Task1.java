f32284c23863edec8af3c72d54e280d9d0905add
Egor_Shokhalevich <ioga@bk.ru>
2022-01-30 12:22:00 +0300

    Day 5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,47 @@
 package day5;
 
+//1. Создать класс Автомобиль (англ. Car) с полями “Модель”, “Цвет”, “Год выпуска”.
+// Создать get и set методы для каждого поля. Создать экземпляр класса Автомобиль.
+// Задать значение для каждого поля, используя set методы.
+// Вывести в консоль значение каждого из полей, используя get методы.
+//Созданный вами класс должен отвечать принципам инкапсуляции.
+
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("Lada");
+        car.setColor("Red");
+        car.setYear(1989);
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+    }
+}
 
+class Car{
+    private String model;
+    private String color;
+    private int year;
+
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+    public int getYear(){
+        return year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,55 @@
 package day5;
 
+//2. Создать класс Мотоцикл (англ. Motorbike) с полями “Модель”, “Цвет”, “Год выпуска”.
+// Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+// Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+// Вывести в консоль значение каждого из полей, используя get методы.
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Java", "RED", 1987);
+        System.out.println(motorbike.getModel());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getYear());
+    }
+}
 
+class Motorbike {
+    private String model;
+    private String color;
+    private int year;
+
+    public Motorbike(){
+        System.out.println("Jopa");
+    }
+
+    public Motorbike(String model, String color, int year){
+        this.model = model;
+        this.color = color;
+        this.year = year;
+
+    }
+
+
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+    public int getYear(){
+        return year;
     }
 }
