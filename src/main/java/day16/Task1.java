a9db0794246e742ac76977e5dccd02a3901f3198
Egor_Shokhalevich <ioga@bk.ru>
2022-02-09 22:33:17 +0300

    Day 16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,46 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file=new File("E:\\JavaMarathon2021\\numbers.txt");
+        printResult(file);
+
+    }
+
+
+    static void printResult (File file){
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbersAsString = line.split(" ");
+
+           if (numbersAsString.length==0){
+               throw new IllegalArgumentException();
+          }
+
+            Double averageNum = 0.0;
+            int count = 0;
+            for (String number:numbersAsString) {
+                averageNum+=Double.parseDouble(number);
+                count++;
+            }
+
+            String s2 = String.format(Locale.ROOT, "%.3f",(averageNum/count));  //локаль для вывода точки вместо запятой в десятичном разделителе
+
+            System.out.println((averageNum/count) + " --> " + s2);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e){
+            System.out.println("Некорректный или пустой входной файл");
+        }
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,133 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        CreateNewFile("file1");
+        CreateNewFile("file2");
+        addThousandRandomNumbersToFile1(new File("E:\\JavaMarathon2021\\file1.txt"));
+        addNumbersToFile2(new File("E:\\JavaMarathon2021\\file1.txt"));
+        printResult(new File("E:\\JavaMarathon2021\\file2.txt"));
+
+    }
+
+    static void addThousandRandomNumbersToFile1(File file) {
+        Random random = new Random();
+
+        try {
+            PrintWriter pw = new PrintWriter(file);
+
+            for (int i = 0; i < 1000; i++) {
+                int z = random.nextInt(100);
+                pw.print(z + " ");
+            }
+
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("addThousandRandomNumbersToFile1. Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("addThousandRandomNumbersToFile1. Некорректный входной файл");
+        }
+
+
+    }
+//Метод заполнения Файла 2 следующий: для каждой группы из 20 целых чисел из
+// Файла 1 рассчитывается их среднее арифметическое. Затем, полученное значение записывается в Файл 2.
+// Таким образом в Файле 2 будет находиться 50 вещественных чисел (1000 / 20 = 50).
+
+
+    static void addNumbersToFile2(File file) {
+        try {
+
+            int count = 0;
+            double averageTwenty = 0;
+
+            Scanner scanner = new Scanner(file);
+            File outputFile = new File("E:\\JavaMarathon2021\\file2.txt");
+            PrintWriter pw = new PrintWriter(outputFile);
+
+
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                String[] numbersStringArray = line.split("\r");
+
+                for (String s : numbersStringArray) {
+                    averageTwenty += Integer.parseInt(s);
+                    count++;
+                    if (count == 20) {
+
+                        pw.print((averageTwenty / 20.0) + " ");
+                        //System.out.println(averageTwenty/20.0);
+                        count = 0;
+                    }
+                }
+            }
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("addNumbersToFile2. Файл не найден");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("addNumbersToFile2. Некорректный входной файл");
+            e.printStackTrace();
+
+        }
+
+
+    }
+//статический метод printResult(File file).
+// Этот метод должен рассчитать сумму всех вещественных чисел из Файла 2 и вывести её в консоль, отбросив вещественную часть.
+
+    static void printResult(File file) {
+        try {
+            double sumAllNumsInFile2 = 0;
+            double averageFile2 = 0;
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.next();
+                String[] numbersStringArray = line.split("\r");
+
+                for (String s : numbersStringArray) {
+                    sumAllNumsInFile2 += Double.parseDouble(s);
+                }
+                averageFile2 = sumAllNumsInFile2 / numbersStringArray.length;
+            }
+
+            System.out.println((int)Math.ceil(averageFile2));
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("printResult. Файл не найден");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("printResult. Некорректный входной файл");
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+    static void CreateNewFile(String filename) {
+        try {
+            File file = new File("E:\\JavaMarathon2021\\" + filename + ".txt");
+            if (file.createNewFile()) {
+                System.out.println("Мы создали новый файл: " + file.getName());
+            } else {
+                System.out.println("Файл уже создан до нас.");
+            }
+        } catch (IOException e) {
+            System.out.println("Что-то пошло не так при создании файла.");
+        }
+
 
     }
 }
